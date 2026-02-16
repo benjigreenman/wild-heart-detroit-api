424857c06dd33cecbbb24d141f27afa736fd02b0
benjigreenman <75400075+benjigreenman@users.noreply.github.com>
2026-02-16 13:01:55 -0500

    Initial golang api

--- internal/provider/spotify.go
+++ internal/provider/spotify.go
@@ -27,15 +27,5 @@ func (s *SpotifyProvider) GetShows(ctx context.Context, limit int) ([]MediaItem,
 
 func (y *SpotifyProvider) mapEpisodes(eps []MediaItem) ([]MediaItem, error) {
 	// TODO: Implement real API call using y.APIKey
-	return []MediaItem{
-		{
-			ID:          "1",
-			Title:       "Spotify Episode 1",
-			ImageURL:    "https://example.com/image1.jpg",
-			ExternalURL: "https://spotify.com/ep1",
-			DurationMs:  3600000,
-			Source:      "spotify",
-			ReleaseDate: time.Now().AddDate(0, 0, -1),
-		},
-	}, nil
+	return eps, nil
 }

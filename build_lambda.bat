2e28590c9e02ee301703855db9b095e2d0c0e3ef
benjigreenman <75400075+benjigreenman@users.noreply.github.com>
2026-02-25 20:33:35 -0500

    Fixes to the builds

--- build_lambda.bat
+++ build_lambda.bat
@@ -2,14 +2,16 @@
 REM Build the lambda handler for deployment from the project root
 cd /d %~dp0\cmd\lambda
 
-$env:GOOS="linux"
-$env:GOARCH="amd64"
+
+set GOOS=linux
+set GOARCH=amd64
 go build -o bootstrap main.go
 
 if %errorlevel% neq 0 (
     echo Build failed.
     exit /b %errorlevel%
 )
+
 
 tar.exe -a -c -f function.zip bootstrap
 
